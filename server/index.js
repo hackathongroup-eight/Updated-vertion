0a74387c58050a6a6b24c85828fa53a1135b72a1
medendale <70830116+medendale@users.noreply.github.com>
2021-12-22 01:44:31 -0500

    reinitialized files

---
+++ server/index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+// app.get('/', (req, res)=>{
+//     res.send('Hello to Memories API');
+// });
+
+// const CONNECTION_URL = 'mongodb+srv://med:mutagousa@cluster0.eieym.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port:http://localhost: ${PORT}`)))
+    // .catch(err => console.log(err));
+
+// mongoose.set('useCreateIndex', true);
